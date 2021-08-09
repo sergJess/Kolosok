12646b5c82c561cda372ad98c8ffddcdd0eadd99
sergJess <serg.fantasy@yandex.ru>
2021-08-09 11:22:17 +0300

    feat: add section form

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,18 @@ gulp.task('scss-3', function() {
         .pipe(gulp.dest('css'));
 });
 
+gulp.task('scss-4', function() {
+    return gulp.src('scss/style-page-feedback.scss')
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(autoprefixer({
+                overrideBrowserslist: ['last 10 versions'],
+                cascade: false
+            }
+
+        ))
+        .pipe(gulp.dest('css'));
+});
+
 gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', gulp.parallel('scss','scss-2','scss-3'));
+    gulp.watch('scss/**/*.scss', gulp.parallel('scss','scss-2','scss-3','scss-4'));
 })
