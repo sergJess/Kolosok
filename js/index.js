3c3d6e7f4ee8b6b881948f8a6dae53bdb17900b6
sergJess <serg.fantasy@yandex.ru>
2021-08-09 13:38:34 +0300

    feat: add gradient and form

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // nav
 const navigation =  document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav__link');
-navigation.addEventListener('click',(e)=>{
+navigation.addEventListener('mouseover',(e)=>{
   if(e.target.classList.contains('nav__link')){
   for(let i=0, length = navLinks.length; i<length;i++){
     navLinks[i].classList.remove('nav__link_active');
@@ -11,3 +11,16 @@ navigation.addEventListener('click',(e)=>{
   e.target.parentNode.classList.add('nav__list_active');
   }
 });
+
+//gradient
+
+const header = document.querySelector('.header');
+const introduce = document.querySelector('.introduce');
+const gradientLine = document.querySelector('.gradient-line');
+const gradientHeight = getSumOfHeights(header, introduce);
+gradientLine.style.height = `${getSumOfHeights(header, introduce)}px`;
+function getSumOfHeights(element1, element2){
+  return element1.clientHeight + element2.clientHeight;
+}
+
+console.log(gradientHeight);
